abf7e7e73b947c9dddd3c207a5e73bf46f2fe3e4
MB20 JCLiu <JCLiu@nuvoton.com>
2022-05-22 10:31:07 +0800

    first commit

---
+++ SampleCode/StdDriver/unity/main.c
@@ -0,0 +1,101 @@
+/**************************************************************************//**
+ * @file     main.c
+ * @version  V3.00
+ * @brief    Show the usage of GPIO interrupt function.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * @copyright (C) 2018 Nuvoton Technology Corp. All rights reserved.
+ ******************************************************************************/
+#include <stdio.h>
+#include "NuMicro.h"
+
+#include "unity.h"
+void SYS_Init(void)
+{
+    /* Unlock protected registers */
+    SYS_UnlockReg();
+
+    /* Enable HIRC */
+    CLK_EnableXtalRC(CLK_PWRCTL_HIRCEN_Msk);
+
+    /* Waiting for HIRC clock ready */
+    CLK_WaitClockReady(CLK_STATUS_HIRCSTB_Msk);
+
+    /* Switch HCLK clock source to HIRC */
+    CLK_SetHCLK(CLK_CLKSEL0_HCLKSEL_HIRC, CLK_CLKDIV0_HCLK(1));
+
+    /* Set both PCLK0 and PCLK1 as HCLK/2 */
+    CLK->PCLKDIV = (CLK_PCLKDIV_APB0DIV_DIV2 | CLK_PCLKDIV_APB1DIV_DIV2);
+
+    /* Switch UART0 clock source to HIRC */
+    CLK_SetModuleClock(UART0_MODULE, CLK_CLKSEL1_UART0SEL_HIRC, CLK_CLKDIV0_UART0(1));
+
+    /* Enable UART peripheral clock */
+    CLK_EnableModuleClock(UART0_MODULE);
+
+    /* Update System Core Clock */
+    /* User can use SystemCoreClockUpdate() to calculate PllClock, SystemCoreClock and CycylesPerUs automatically. */
+    SystemCoreClockUpdate();
+
+    /*----------------------------------------------------------------------*/
+    /* Init I/O Multi-function                                              */
+    /*----------------------------------------------------------------------*/
+    /* Set GPB multi-function pins for UART0 RXD and TXD */
+    SYS->GPB_MFPH = (SYS->GPB_MFPH & ~(SYS_GPB_MFPH_PB12MFP_Msk | SYS_GPB_MFPH_PB13MFP_Msk)) |
+                    (SYS_GPB_MFPH_PB12MFP_UART0_RXD | SYS_GPB_MFPH_PB13MFP_UART0_TXD);
+
+    /* Lock protected registers */
+    SYS_LockReg();
+}
+
+/*----------------------------------------------------------------------*/
+/* Init UART0                                                           */
+/*----------------------------------------------------------------------*/
+void UART0_Init(void)
+{
+    /* Reset UART0 */
+    SYS_ResetModule(UART0_RST);
+
+    /* Configure UART0 and set UART0 baud rate */
+    UART_Open(UART0, 115200);
+}
+void setUp(void)
+{
+	printf("a\n\r");
+}
+ 
+void tearDown(void)
+{
+   printf("b\n\r");
+}
+
+ 
+void test1(void)
+{
+	TEST_ASSERT_TRUE(1);
+	TEST_ASSERT_TRUE(1);
+}
+ 
+void test2(void)
+{
+	TEST_ASSERT_FALSE(0);
+	TEST_ASSERT_FALSE(1);
+}
+
+int main(void)
+{
+    /* Init System, IP clock and multi-function I/O. */
+    SYS_Init();
+
+    /* Init UART0 for printf */
+    UART0_Init();
+
+    printf("\n\nCPU @ %d Hz\n", SystemCoreClock);
+    ASSERT_PARAM(0);
+	  RUN_TEST(test1);
+	  RUN_TEST(test2);
+	  UNITY_END();
+
+	
+    while(1);
+}
